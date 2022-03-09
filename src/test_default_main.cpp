2beb0bdbfedfecff3195fb941db8368a421745e4
ThatColdToast <72617136+ThatColdToast@users.noreply.github.com>
2022-03-09 13:28:21 -0600

    New main.cpp

--- src/test_default_main.cpp
+++ src/test_default_main.cpp
@@ -37,11 +37,11 @@ static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int acti
 		s_showStats = !s_showStats;
 }
 
-int main(int argc, char** argv)
+int test_default_main(int argc, char** argv)
 {
 	// Create a GLFW window without an OpenGL context.
 	glfwSetErrorCallback(glfw_errorCallback);
-
+		
 	if (!glfwInit())
 		return 1;
 
@@ -75,7 +75,6 @@ int main(int argc, char** argv)
 	init.resolution.width = (uint32_t)width;
 	init.resolution.height = (uint32_t)height;
 	init.resolution.reset = BGFX_RESET_VSYNC;
-
 	if (!bgfx::init(init))
 		return 1;
 
@@ -103,9 +102,13 @@ int main(int argc, char** argv)
 
 		// Use debug font to print information about this example.
 		bgfx::dbgTextClear();
-
+		bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(width / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
+		bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
+		bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+		bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+		bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
 		const bgfx::Stats* stats = bgfx::getStats();
-
+		bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
 		// Enable stats or debug text.
 		bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
 		// Advance to next frame. Process submitted rendering primitives.
